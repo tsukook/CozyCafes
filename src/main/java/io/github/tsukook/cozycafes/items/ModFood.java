da536424cea43ec7d8ed1c922184d970a87198b1
PouPeuu <qwertydude277@gmail.com>
2024-04-13 14:22:54 +0300

    Change Ground Coffee effect to Caffeinated

--- src/main/java/io/github/tsukook/cozycafes/items/ModFood.java
+++ src/main/java/io/github/tsukook/cozycafes/items/ModFood.java
@@ -1,10 +1,10 @@
 package io.github.tsukook.cozycafes.items;
 
+import io.github.tsukook.cozycafes.effects.Effects;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
 
 public class ModFood {
-    // replace MOVEMENT_SPEED with CAFFIENATED once the effect is added! :3c
-    public static final FoodProperties GROUNDCOFFEE = new FoodProperties.Builder().alwaysEat().fast().effect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 600, 1), 1f).build();
+    public static final FoodProperties GROUNDCOFFEE = new FoodProperties.Builder().alwaysEat().fast().effect(new MobEffectInstance(Effects.CAFFEINATED.get(), 600, 1), 1f).build();
 }
