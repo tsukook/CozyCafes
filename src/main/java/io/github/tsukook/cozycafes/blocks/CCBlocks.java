6dd8e8a6d3d851779f69eded09ac486e5d09bd66
GingerIndustries <75683114+GingerIndustries@users.noreply.github.com>
2024-04-22 12:25:34 -0400

    Add `lang()` calls to the blocks
    This might be unnecessary? I think Registrate automatically creates them from the ID.

--- src/main/java/io/github/tsukook/cozycafes/blocks/CCBlocks.java
+++ src/main/java/io/github/tsukook/cozycafes/blocks/CCBlocks.java
@@ -9,10 +9,12 @@ import static io.github.tsukook.cozycafes.CozyCafes.REGISTRATE;
 public class CCBlocks {
     public static final BlockEntry<CoffeeBushBlock> COFFEE_BUSH = REGISTRATE.block("coffee_bush", CoffeeBushBlock::new)
             .initialProperties(() -> Blocks.SWEET_BERRY_BUSH)
+            .lang("Coffee Bush")
             .simpleItem()
             .register();
     public static final BlockEntry<Block> COLD_BREWER = REGISTRATE.block("cold_brewer", Block::new)
             .initialProperties(() -> Blocks.GLASS)
+            .lang("Cold Brewer")
             .simpleItem()
             .register();
 
