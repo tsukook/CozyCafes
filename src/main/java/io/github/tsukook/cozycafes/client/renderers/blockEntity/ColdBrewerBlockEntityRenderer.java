053fa05b162f2d5f7b4966b8713e35136b21a7c8
PouPeuu <qwertydude277@gmail.com>
2024-04-24 15:48:56 +0300

    Make the fluid go inside the jar

--- src/main/java/io/github/tsukook/cozycafes/client/renderers/blockEntity/ColdBrewerBlockEntityRenderer.java
+++ src/main/java/io/github/tsukook/cozycafes/client/renderers/blockEntity/ColdBrewerBlockEntityRenderer.java
@@ -6,10 +6,12 @@ import io.github.tsukook.cozycafes.blocks.entities.ColdBrewerBlockEntity;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
+import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraftforge.fluids.FluidStack;
 
 public class ColdBrewerBlockEntityRenderer implements BlockEntityRenderer<ColdBrewerBlockEntity> {
     public ColdBrewerBlockEntityRenderer(BlockEntityRendererProvider.Context context) {}
+    private static final float PIXEL = 1f/16f;
 
     @Override
     public void render(ColdBrewerBlockEntity coldBrewerBlockEntity, float partialTick, PoseStack poseStack, MultiBufferSource multiBufferSource, int packedLight, int packedOverlay) {
@@ -20,7 +22,7 @@ public class ColdBrewerBlockEntityRenderer implements BlockEntityRenderer<ColdBr
         FluidStack fluidStack = coldBrewerBlockEntity.getFluid().getFluid();
 
         if (!fluidStack.isEmpty()) {
-            FluidRenderer.renderFluidBox(fluidStack, 0, 0, 0, 1, 1, 1, multiBufferSource, poseStack, packedLight, true);
+            FluidRenderer.renderFluidBox(fluidStack, PIXEL*3+0.001f, 0, PIXEL*3+0.001f, 1-PIXEL*3-0.001f, 1-PIXEL*6, 1-PIXEL*3-0.001f, multiBufferSource, poseStack, packedLight, true);
         }
     }
 }
