d73b15170a98fcecc771033b85827215e60833e5
PouPeuu <qwertydude277@gmail.com>
2024-05-02 08:23:25 +0300

    Add ability to pick up mug by right clicking MugBlock with an empty hand

--- src/main/java/io/github/tsukook/cozycafes/blocks/MugBlock.java
+++ src/main/java/io/github/tsukook/cozycafes/blocks/MugBlock.java
@@ -3,19 +3,48 @@ package io.github.tsukook.cozycafes.blocks;
 import io.github.tsukook.cozycafes.blocks.entities.MugBlockEntity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraftforge.fluids.FluidStack;
 
 public class MugBlock extends BaseEntityBlock {
     public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
     public MugBlock(Properties pProperties) {
         super(pProperties);
         registerDefaultState(getStateDefinition().any().setValue(FACING, Direction.NORTH));
+    }
+
+    @Override
+    public InteractionResult use(BlockState blockState, Level level, BlockPos blockPos, Player player, InteractionHand interactionHand, BlockHitResult blockHitResult) {
+        ItemStack itemStack = player.getItemInHand(interactionHand);
+        if (itemStack.isEmpty()) {
+            MugBlockEntity mugBlockEntity = (MugBlockEntity) level.getBlockEntity(blockPos);
+            FluidStack fluidStack = mugBlockEntity.getFluid();
+            CompoundTag compoundTag = new CompoundTag();
+
+            fluidStack.writeToNBT(compoundTag);
+            ItemStack mugItem = new ItemStack(CCBlocks.MUG_BLOCK.asItem(), 1);
+            mugItem.setTag(compoundTag);
+
+            player.setItemInHand(interactionHand, mugItem);
+
+            level.removeBlock(blockPos, false);
+            return InteractionResult.sidedSuccess(level.isClientSide());
+        }
+
+        return InteractionResult.PASS;
     }
 
     @Override
