b6f836ccc4e7816c141186eb4269e8c924441217
PouPeuu <qwertydude277@gmail.com>
2024-05-05 17:55:57 +0300

    Change MugBlock to extend PickupableBaseEntityBlock

--- src/main/java/io/github/tsukook/cozycafes/blocks/MugBlock.java
+++ src/main/java/io/github/tsukook/cozycafes/blocks/MugBlock.java
@@ -3,26 +3,18 @@ package io.github.tsukook.cozycafes.blocks;
 import io.github.tsukook.cozycafes.blocks.entities.MugBlockEntity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
-import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
-import net.minecraftforge.fluids.FluidStack;
 
-public class MugBlock extends BaseEntityBlock {
+public class MugBlock extends PickupableBaseEntityBlock {
     public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
     private static final VoxelShape SHAPE = Block.box(6, 0, 6, 10, 5, 10);
 
@@ -34,27 +26,6 @@ public class MugBlock extends BaseEntityBlock {
     @Override
     public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
         return SHAPE;
-    }
-
-    @Override
-    public InteractionResult use(BlockState blockState, Level level, BlockPos blockPos, Player player, InteractionHand interactionHand, BlockHitResult blockHitResult) {
-        ItemStack itemStack = player.getItemInHand(interactionHand);
-        if (itemStack.isEmpty()) {
-            MugBlockEntity mugBlockEntity = (MugBlockEntity) level.getBlockEntity(blockPos);
-            FluidStack fluidStack = mugBlockEntity.getFluid();
-            CompoundTag compoundTag = new CompoundTag();
-
-            fluidStack.writeToNBT(compoundTag);
-            ItemStack mugItem = new ItemStack(CCBlocks.MUG_BLOCK.asItem(), 1);
-            mugItem.setTag(compoundTag);
-
-            player.setItemInHand(interactionHand, mugItem);
-
-            level.removeBlock(blockPos, false);
-            return InteractionResult.sidedSuccess(level.isClientSide());
-        }
-
-        return InteractionResult.PASS;
     }
 
     @Override
