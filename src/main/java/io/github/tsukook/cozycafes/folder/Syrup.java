b320ec5ed9bb058ad9ae125fe37b84ee1579bc4e
PouPeuu <qwertydude277@gmail.com>
2024-05-29 09:29:08 +0300

    Create Syrup class to hold data about syrups

---
+++ src/main/java/io/github/tsukook/cozycafes/folder/Syrup.java
@@ -0,0 +1,63 @@
+package io.github.tsukook.cozycafes.folder;
+
+import com.simibubi.create.foundation.utility.NBTHelper;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.world.effect.MobEffectInstance;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Syrup {
+    private final ArrayList<MobEffectInstance> mobEffectInstances = new ArrayList<>();
+
+    public Syrup(ArrayList<MobEffectInstance> mobEffectInstances) {
+        this.mobEffectInstances.addAll(mobEffectInstances);
+    }
+
+    public Syrup(MobEffectInstance mobEffectInstance) {
+        this((ArrayList<MobEffectInstance>) Collections.singletonList(mobEffectInstance));
+    }
+
+    public Syrup() {}
+
+    public static Syrup readFromNBT(CompoundTag compoundTag) {
+        if (compoundTag != null && !compoundTag.isEmpty() && compoundTag.contains("PotionEffects")) {
+            ArrayList<MobEffectInstance> mobEffectInstanceList = new ArrayList<>();
+
+            ListTag listTag = compoundTag.getList("PotionEffects", ListTag.TAG_COMPOUND);
+            listTag.forEach(tag -> {
+                MobEffectInstance mobEffectInstance = MobEffectInstance.load((CompoundTag) tag);
+                mobEffectInstanceList.add(mobEffectInstance);
+            });
+
+            return new Syrup(mobEffectInstanceList);
+        }
+
+        return new Syrup();
+    }
+
+    public CompoundTag writeToNBT(CompoundTag compoundTag) {
+        ListTag listTag = new ListTag();
+        for (MobEffectInstance effect : this.mobEffectInstances) {
+            listTag.add(effect.save(new CompoundTag()));
+        }
+
+        compoundTag.put("PotionEffects", listTag);
+
+        return compoundTag;
+    }
+
+    public void addEffect(MobEffectInstance mobEffectInstance) {
+        this.mobEffectInstances.add(mobEffectInstance);
+    }
+
+    public ArrayList<MobEffectInstance> getEffects() {
+        return this.mobEffectInstances;
+    }
+
+    public boolean hasEffects() {
+        return !this.mobEffectInstances.isEmpty();
+    }
+}
