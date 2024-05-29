8fd353701fdc8ddd9630c560e2fa2d432d4657c9
PouPeuu <qwertydude277@gmail.com>
2024-05-29 14:12:32 +0300

    Add SyrupPumpBlock#use to be able to right click with potions to add to syrup

--- src/main/java/io/github/tsukook/cozycafes/blocks/SyrupPumpBlock.java
+++ src/main/java/io/github/tsukook/cozycafes/blocks/SyrupPumpBlock.java
@@ -2,8 +2,15 @@ package io.github.tsukook.cozycafes.blocks;
 
 import io.github.tsukook.cozycafes.blocks.entities.SyrupPumpBlockEntity;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.PotionItem;
+import net.minecraft.world.item.alchemy.PotionUtils;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Mirror;
 import net.minecraft.world.level.block.RenderShape;
@@ -13,6 +20,7 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
@@ -54,5 +62,17 @@ public class SyrupPumpBlock extends PickupableBaseEntityBlock {
     @Override
     public RenderShape getRenderShape(BlockState pState) {
         return RenderShape.MODEL;
+    }
+
+    @Override
+    public InteractionResult use(BlockState blockState, Level level, BlockPos blockPos, Player player, InteractionHand interactionHand, BlockHitResult blockHitResult) {
+        ItemStack itemStack = player.getItemInHand(interactionHand);
+        if (itemStack.getItem() instanceof PotionItem potionItem) {
+            if (level.getBlockEntity(blockPos) instanceof SyrupPumpBlockEntity syrupPumpBlockEntity) {
+                if (syrupPumpBlockEntity.addPotion(PotionUtils.getPotion(itemStack)))
+                    return InteractionResult.sidedSuccess(level.isClientSide());
+            }
+        }
+        return super.use(blockState, level, blockPos, player, interactionHand, blockHitResult);
     }
 }
