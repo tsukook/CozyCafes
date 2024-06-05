931b5f7e35bede31b26d48cc78a50a1c5db0bb0b
PouPeuu <qwertydude277@gmail.com>
2024-06-05 10:54:07 +0300

    Condense coffee fluids to one

--- src/main/java/io/github/tsukook/cozycafes/fluids/CCFluids.java
+++ src/main/java/io/github/tsukook/cozycafes/fluids/CCFluids.java
@@ -1,21 +1,17 @@
 package io.github.tsukook.cozycafes.fluids;
 
-import com.simibubi.create.content.fluids.VirtualFluid;
-import com.simibubi.create.foundation.data.CreateRegistrate;
 import com.tterrag.registrate.util.entry.FluidEntry;
 import io.github.tsukook.cozycafes.CozyCafes;
 
 import static io.github.tsukook.cozycafes.CozyCafes.REGISTRATE;
 
 public class CCFluids {
-    public static final FluidEntry<VirtualFluid> BLOND_ROAST_COLD_BREW = REGISTRATE.virtualFluid("blond_roast_cold_brew", CozyCafes.getResource("fluid/blond_roast_cold_brew_still"), CozyCafes.getResource("fluid/blond_roast_cold_brew_flow"), CreateRegistrate::defaultFluidType, VirtualFluid::new)
-            .register();
-    public static final FluidEntry<VirtualFluid> MEDIUM_ROAST_COLD_BREW = REGISTRATE.virtualFluid("medium_roast_cold_brew", CozyCafes.getResource("fluid/medium_roast_cold_brew_still"), CozyCafes.getResource("fluid/medium_roast_cold_brew_flow"), CreateRegistrate::defaultFluidType, VirtualFluid::new)
-            .register();
-    public static final FluidEntry<VirtualFluid> DARK_ROAST_COLD_BREW = REGISTRATE.virtualFluid("dark_roast_cold_brew", CozyCafes.getResource("fluid/dark_roast_cold_brew_still"), CozyCafes.getResource("fluid/dark_roast_cold_brew_flow"), CreateRegistrate::defaultFluidType, VirtualFluid::new)
+    public static final FluidEntry<CoffeeFluid> COFFEE = REGISTRATE.virtualFluid("coffee", CozyCafes.getResource("fluid/coffee_still"), CozyCafes.getResource("fluid/coffee_flow"), CoffeeFluid.CoffeeFluidType::new, CoffeeFluid::new)
+            .lang("Coffee")
             .register();
 
     public static final FluidEntry<SyrupFluid> SYRUP = REGISTRATE.virtualFluid("syrup", CozyCafes.getResource("fluid/syrup_still"), CozyCafes.getResource("fluid/syrup_flow"), SyrupFluid.SyrupFluidType::new, SyrupFluid::new)
+            .lang("Syrup")
             .register();
 
     @SuppressWarnings("EmptyMethod")
