7975724ee4ab518bdee884f0deda3b394d25074c
PouPeuu <qwertydude277@gmail.com>
2024-08-28 14:19:01 +0300

    Add Forge 1.20.1 and Parchment mappings

---
+++ src/main/java/io/github/tsukook/cozycafes/CozyCafes.java
@@ -0,0 +1,14 @@
+package io.github.tsukook.cozycafes;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(CozyCafes.MODID)
+public class CozyCafes
+{
+    public static final String MODID = "cozycafes";
+
+    public CozyCafes()
+    {
+
+    }
+}
