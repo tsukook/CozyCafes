6aa05f95d9f4668e56869b9e955cacc1d60239d6
PouPeuu <qwertydude277@gmail.com>
2024-08-31 22:33:04 +0300

    Give a model to test container

--- src/main/java/io/github/tsukook/cozycafes/CCBlocks.java
+++ src/main/java/io/github/tsukook/cozycafes/CCBlocks.java
@@ -1,25 +1,19 @@
 package io.github.tsukook.cozycafes;
 
 import com.tterrag.registrate.util.entry.BlockEntry;
-import com.tterrag.registrate.util.entry.RegistryEntry;
 import io.github.tsukook.cozycafes.block.FluidContainerBlock;
-import net.minecraft.world.level.block.Block;
 
 import static io.github.tsukook.cozycafes.CCRegistry.REGISTRATE;
 
 public class CCBlocks {
-    public static final RegistryEntry<Block> TEST_BLOCK = REGISTRATE.block("test_block", Block::new)
-            .simpleItem()
-            .lang("Test Block")
-            .register();
-
     public static final BlockEntry<FluidContainerBlock> TEST_CONTAINER = REGISTRATE.block("test_container", FluidContainerBlock::new)
             /*.blockEntity(((blockEntityType, blockPos, blockState) -> new FluidContainerBlockEntity(blockEntityType, blockPos, blockState, 1000)))
             .build()*/
+            .properties(properties -> properties.noOcclusion())
+            .defaultBlockstate()
             .lang("Test Container")
             .simpleItem()
             .register();
 
-    // don't delete
     public static void register() {}
 }
