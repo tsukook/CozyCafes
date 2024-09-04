0e6fa67aa8757cd779533abdab597ff7cc96bb6a
PouPeuu <qwertydude277@gmail.com>
2024-09-04 15:08:44 +0300

    Add drawFluidQuad to RenderHelper

--- src/main/java/io/github/tsukook/cozycafes/client/renderer/RenderHelper.java
+++ src/main/java/io/github/tsukook/cozycafes/client/renderer/RenderHelper.java
@@ -2,6 +2,15 @@ package io.github.tsukook.cozycafes.client.renderer;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.inventory.InventoryMenu;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraftforge.client.extensions.common.IClientFluidTypeExtensions;
+import net.minecraftforge.fluids.FluidStack;
 
 public class RenderHelper {
     public static void drawVertex(VertexConsumer vertexConsumer, PoseStack poseStack, float x, float y, float z, float u, float v, int packedLight, int color) {
@@ -34,5 +43,27 @@ public class RenderHelper {
         drawQuad(vertexConsumer, poseStack, x1, y0, z0, x1, y1, z1, true, u0, v0, u1, v1, packedLight, color); // purple negative x
         drawQuad(vertexConsumer, poseStack, x0, y0, z1, x1, y0, z0, false, u0, v0, u1, v1, packedLight, color); // yellow y positive y
         drawQuad(vertexConsumer, poseStack, x0, y1, z0, x1, y1, z1, false, u0, v0, u1, v1, packedLight, color); // cyan y negative y
+    }
+
+    public static void drawFluidQuad(FluidStack fluidStack, boolean flowing, MultiBufferSource multiBufferSource, PoseStack poseStack, float x0, float y0, float z0, float x1, float y1, float z1, boolean magic, int packedLight) {
+        IClientFluidTypeExtensions fluidTypeExtensions = IClientFluidTypeExtensions.of(fluidStack.getFluid());
+
+        ResourceLocation fluidTexture;
+        if (flowing) {
+            fluidTexture = fluidTypeExtensions.getFlowingTexture(fluidStack);
+        } else {
+            fluidTexture = fluidTypeExtensions.getStillTexture(fluidStack);
+        }
+
+        if (fluidTexture == null)
+            return;
+
+        FluidState fluidState = fluidStack.getFluid().defaultFluidState();
+
+        TextureAtlasSprite sprite = Minecraft.getInstance().getTextureAtlas(InventoryMenu.BLOCK_ATLAS).apply(fluidTexture);
+        int tintColor = fluidTypeExtensions.getTintColor(fluidStack);
+        VertexConsumer vertexConsumer = multiBufferSource.getBuffer(ItemBlockRenderTypes.getRenderLayer(fluidState));
+
+        drawQuad(vertexConsumer, poseStack, x0, y0, z0, x1, y1, z1, magic, sprite.getU0(), sprite.getV0(), sprite.getU1(), sprite.getV1(), packedLight, tintColor);
     }
 }
