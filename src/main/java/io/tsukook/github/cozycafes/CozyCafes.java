89c5822244d635b8a1895c91506207f1ad38938c
PouPeuu <qwertydude277@gmail.com>
2025-03-05 15:53:00 +0200

    Create Creative Tab Registry

--- src/main/java/io/tsukook/github/cozycafes/CozyCafes.java
+++ src/main/java/io/tsukook/github/cozycafes/CozyCafes.java
@@ -1,6 +1,7 @@
 package io.tsukook.github.cozycafes;
 
 import io.tsukook.github.cozycafes.registers.BlockRegistry;
+import io.tsukook.github.cozycafes.registers.CreativeTabRegistry;
 import io.tsukook.github.cozycafes.registers.ItemRegistry;
 import org.slf4j.Logger;
 
@@ -8,12 +9,6 @@ import com.mojang.logging.LogUtils;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Blocks;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
@@ -27,40 +22,13 @@ import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
-import net.neoforged.neoforge.registries.DeferredHolder;
-import net.neoforged.neoforge.registries.DeferredItem;
-import net.neoforged.neoforge.registries.DeferredRegister;
 
-// The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(CozyCafes.MODID)
 public class CozyCafes
 {
-    // Define mod id in a common place for everything to reference
     public static final String MODID = "cozycafes";
-    // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
-    // Create a Deferred Register to hold Blocks which will all be registered under the "cozycafes" namespace
-    //public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(MODID);
-    // Create a Deferred Register to hold Items which will all be registered under the "cozycafes" namespace
-    //public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MODID);
-    // Create a Deferred Register to hold CreativeModeTabs which will all be registered under the "cozycafes" namespace
-    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
 
-    // Creates a new food item with the id "cozycafes:example_id", nutrition 1 and saturation 2
-    //public static final DeferredItem<Item> EXAMPLE_ITEM = ITEMS.registerSimpleItem("example_item", new Item.Properties().food(new FoodProperties.Builder()
-    //        .alwaysEdible().nutrition(1).saturationModifier(2f).build()));
-
-    // Creates a creative tab with the id "cozycafes:example_tab" for the example item, that is placed after the combat tab
-    //public static final DeferredHolder<CreativeModeTab, CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("example_tab", () -> CreativeModeTab.builder()
-    //        .title(Component.translatable("itemGroup.cozycafes")) //The language key for the title of your CreativeModeTab
-    //        .withTabsBefore(CreativeModeTabs.COMBAT)
-    //        .icon(() -> EXAMPLE_ITEM.get().getDefaultInstance())
-    //        .displayItems((parameters, output) -> {
-    //            output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
-    //        }).build());
-
-    // The constructor for the mod class is the first code that is run when your mod is loaded.
-    // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
     public CozyCafes(IEventBus modEventBus, ModContainer modContainer)
     {
         // Register the commonSetup method for modloading
@@ -68,8 +36,7 @@ public class CozyCafes
 
         BlockRegistry.register(modEventBus);
         ItemRegistry.register(modEventBus);
-        // Register the Deferred Register to the mod event bus so tabs get registered
-        CREATIVE_MODE_TABS.register(modEventBus);
+        CreativeTabRegistry.register(modEventBus);
 
         // Register ourselves for server and other game events we are interested in.
         // Note that this is necessary if and only if we want *this* class (CozyCafes) to respond directly to events.
---
+++ src/main/java/io/tsukook/github/cozycafes/registers/CreativeTabRegistry.java
@@ -0,0 +1,27 @@
+package io.tsukook.github.cozycafes.registers;
+
+import io.tsukook.github.cozycafes.CozyCafes;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class CreativeTabRegistry {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, CozyCafes.MODID);
+
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> COZY_CAFES_TAB = CREATIVE_MODE_TABS.register("cozy_cafes_tab", () -> CreativeModeTab.builder()
+            .title(Component.translatable("itemGroup.cozycafes"))
+            .withTabsBefore(CreativeModeTabs.COMBAT)
+            .icon(() -> ItemRegistry.TEST_ITEM.get().getDefaultInstance())
+            .displayItems((parameters, output) -> {
+                output.accept(ItemRegistry.TEST_ITEM.get());
+                output.accept(ItemRegistry.TEST_BLOCK_ITEM.get());
+            }).build());
+
+    public static void register(IEventBus modEventBus) {
+        CREATIVE_MODE_TABS.register(modEventBus);
+    }
+}
