52b99a2a41f6cfe34b1479b0b8ad3a66c9fc6149
PouPeuu <qwertydude277@gmail.com>
2025-03-14 18:27:00 +0200

    Add pulped coffee bean

--- src/main/java/io/tsukook/github/cozycafes/registers/CzCItemRegistry.java
+++ src/main/java/io/tsukook/github/cozycafes/registers/CzCItemRegistry.java
@@ -15,6 +15,7 @@ public class CzCItemRegistry {
     public static final DeferredItem<BlockItem> COFFEE_PLANT = ITEMS.registerSimpleBlockItem(CzCBlockRegistry.COFFEE_PLANT);
     public static final DeferredItem<BlockItem> COFFEE_PULPER = ITEMS.registerSimpleBlockItem(CzCBlockRegistry.COFFEE_PULPER);
     public static final DeferredItem<Item> COFFEE_BERRY = ITEMS.registerSimpleItem("coffee_berry", new Item.Properties());
+    public static final DeferredItem<Item> PULPED_COFFEE_BEAN = ITEMS.registerSimpleItem("pulped_coffee_bean");
 
     public static void register(IEventBus modEventBus) {
         ITEMS.register(modEventBus);
