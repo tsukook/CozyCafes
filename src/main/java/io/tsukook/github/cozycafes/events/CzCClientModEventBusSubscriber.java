e507fea99864a6028d5e69c0dde24b97a3be5373
PouPeuu <qwertydude277@gmail.com>
2025-04-01 17:13:45 +0300

    Show water in WaterFernebnationvat
    Thx to siepert for telling me about BlockColor

--- src/main/java/io/tsukook/github/cozycafes/events/CzCClientModEventBusSubscriber.java
+++ src/main/java/io/tsukook/github/cozycafes/events/CzCClientModEventBusSubscriber.java
@@ -5,13 +5,16 @@ import io.tsukook.github.cozycafes.client.models.CoffeePulperCylinderModel;
 import io.tsukook.github.cozycafes.client.renderers.CoffeePulperBlockEntityRenderer;
 import io.tsukook.github.cozycafes.client.renderers.WaterFermentationVatRenderer;
 import io.tsukook.github.cozycafes.registers.CzCBlockEntityRegistry;
+import io.tsukook.github.cozycafes.registers.CzCBlockRegistry;
+import net.minecraft.client.renderer.BiomeColors;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.EntityRenderersEvent;
+import net.neoforged.neoforge.client.event.RegisterColorHandlersEvent;
 
 @EventBusSubscriber(modid = CozyCafes.MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-public class CzCClientEventSubscriber {
+public class CzCClientModEventBusSubscriber {
     @SubscribeEvent
     public static void registerRenderers(EntityRenderersEvent.RegisterRenderers event) {
         event.registerBlockEntityRenderer(CzCBlockEntityRegistry.COFFEE_PULPER_BLOCK_ENTITY.get(), CoffeePulperBlockEntityRenderer::new);
@@ -21,5 +24,13 @@ public class CzCClientEventSubscriber {
     @SubscribeEvent
     public static void registerLayers(EntityRenderersEvent.RegisterLayerDefinitions event) {
         event.registerLayerDefinition(CoffeePulperCylinderModel.LAYER_LOCATION, CoffeePulperCylinderModel::createBodyLayer);
+    }
+
+    @SubscribeEvent
+    public static void registerBlockColors(RegisterColorHandlersEvent.Block event) {
+        event.register(
+                ((state, blockAndTintGetter, pos, tintIndex) -> blockAndTintGetter != null && pos != null ? BiomeColors.getAverageWaterColor(blockAndTintGetter, pos) : -1),
+                CzCBlockRegistry.WATER_FERMENTATION_VAT.get()
+        );
     }
 }
