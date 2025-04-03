36f075de7a53825ea59b6da646b5a879ec8537ec
PouPeuu <qwertydude277@gmail.com>
2025-04-03 17:07:26 +0300

    Add fermentation

--- src/main/java/io/tsukook/github/cozycafes/blocks/WaterFermentationVat.java
+++ src/main/java/io/tsukook/github/cozycafes/blocks/WaterFermentationVat.java
@@ -2,10 +2,12 @@ package io.tsukook.github.cozycafes.blocks;
 
 import com.mojang.serialization.MapCodec;
 import io.tsukook.github.cozycafes.blocks.entities.WaterFermentationVatBlockEntity;
+import io.tsukook.github.cozycafes.registers.CzCBlockEntityRegistry;
 import io.tsukook.github.cozycafes.registers.CzCItemRegistry;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
@@ -14,6 +16,8 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
@@ -73,6 +77,20 @@ public class WaterFermentationVat extends BaseEntityBlock {
             stack.setCount(waterFermentationVatBlockEntity.addBeans(stack.getCount()));
             return InteractionResult.SUCCESS;
         }
+        return InteractionResult.TRY_WITH_EMPTY_HAND;
+    }
+
+    @Override
+    protected InteractionResult useWithoutItem(BlockState state, Level level, BlockPos pos, Player player, BlockHitResult hitResult) {
+        if (level.getBlockEntity(pos) instanceof WaterFermentationVatBlockEntity waterFermentationVatBlockEntity && player.getItemInHand(InteractionHand.MAIN_HAND).isEmpty()) {
+            level.addFreshEntity(new ItemEntity(level, pos.getX() + 0.5, pos.getY() + 1, pos.getZ() + 0.5, waterFermentationVatBlockEntity.takeBeans(WaterFermentationVatBlockEntity.MAX_BEANS)));
+            return InteractionResult.SUCCESS;
+        }
         return InteractionResult.PASS;
+    }
+
+    @Override
+    public @Nullable <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState state, BlockEntityType<T> blockEntityType) {
+        return createTickerHelper(blockEntityType, CzCBlockEntityRegistry.WATER_FERMENTATION_VAT_BLOCK_ENTITY.get(), level.isClientSide() ? null : WaterFermentationVatBlockEntity::serverTick);
     }
 }
--- src/main/java/io/tsukook/github/cozycafes/blocks/entities/WaterFermentationVatBlockEntity.java
+++ src/main/java/io/tsukook/github/cozycafes/blocks/entities/WaterFermentationVatBlockEntity.java
@@ -5,13 +5,13 @@ import io.tsukook.github.cozycafes.registers.CzCItemRegistry;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class WaterFermentationVatBlockEntity extends BlockEntity {
+    public static final int MAX_BEANS = 64;
     private ItemStack beans = ItemStack.EMPTY;
     private static final int MAX_FERMENTATION_TIME_TICKS = 10 * 20;
     private int fermentation = 0;
@@ -23,23 +23,30 @@ public class WaterFermentationVatBlockEntity extends BlockEntity {
     public int addBeans(int count) {
         if (beans.isEmpty()) {
             beans = new ItemStack(CzCItemRegistry.PULPED_COFFEE_BEAN.get());
-            int real_count = Math.min(64, count);
+            int real_count = Math.min(MAX_BEANS, count);
             int remaining = count - real_count;
             beans.setCount(real_count);
             setChanged();
             return remaining;
         }
 
-        int real_count = Math.min(64, count + beans.getCount());
+        int real_count = Math.min(MAX_BEANS, count + beans.getCount());
         int remaining = count - real_count;
         beans.grow(count);
         setChanged();
         return remaining;
     }
 
+    public ItemStack takeBeans(int amount) {
+        setChanged();
+        return beans.split(amount);
+    }
+
     public static void serverTick(Level level, BlockPos pos, BlockState state, WaterFermentationVatBlockEntity blockEntity) {
-        if (blockEntity.beans.isEmpty())
+        if (!blockEntity.beans.is(CzCItemRegistry.PULPED_COFFEE_BEAN.get())) {
+            blockEntity.fermentation = 0;
             return;
+        }
         blockEntity.fermentation += 1;
 
         if (blockEntity.fermentation >= MAX_FERMENTATION_TIME_TICKS) {
