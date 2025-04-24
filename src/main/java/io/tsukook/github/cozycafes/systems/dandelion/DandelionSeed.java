c8e04810eb3acbcace7786e5a65b42f26c658ebc
PouPeuu <qwertydude277@gmail.com>
2025-04-24 16:43:54 +0300

    Add id and helper constructors to DandelionSeed

--- src/main/java/io/tsukook/github/cozycafes/systems/dandelion/DandelionSeed.java
+++ src/main/java/io/tsukook/github/cozycafes/systems/dandelion/DandelionSeed.java
@@ -3,13 +3,34 @@ package io.tsukook.github.cozycafes.systems.dandelion;
 import org.joml.Vector3f;
 
 public class DandelionSeed {
+    private static int nextId = 0;
+    private int id;
     public Vector3f pos;
     public Vector3f velocity;
 
-    public DandelionSeed() {}
+    public DandelionSeed(DandelionSeed original) {
+        this(original.getId(), new Vector3f(original.pos), new Vector3f(original.velocity));
+    }
+
+    public DandelionSeed() {
+        this(new Vector3f(), new Vector3f());
+    }
+
+    public DandelionSeed(Vector3f pos) {
+        this(pos, new Vector3f());
+    }
 
     public DandelionSeed(Vector3f pos, Vector3f velocity) {
+        this(nextId++, pos, velocity);
+    }
+
+    public DandelionSeed(int id, Vector3f pos, Vector3f velocity) {
+        this.id = id;
         this.pos = pos;
         this.velocity = velocity;
+    }
+
+    public int getId() {
+        return id;
     }
 }
