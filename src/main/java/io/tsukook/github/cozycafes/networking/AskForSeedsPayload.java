cb9cbf4be1d4ec159d01d2e7b2d4964a7fd0e7e5
PouPeuu <qwertydude277@gmail.com>
2025-04-29 18:32:37 +0300

    Remove params from AskForSeedsPayload

--- src/main/java/io/tsukook/github/cozycafes/networking/AskForSeedsPayload.java
+++ src/main/java/io/tsukook/github/cozycafes/networking/AskForSeedsPayload.java
@@ -2,29 +2,22 @@ package io.tsukook.github.cozycafes.networking;
 
 import io.netty.buffer.ByteBuf;
 import io.tsukook.github.cozycafes.CozyCafes;
-import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.resources.ResourceLocation;
 
-import java.nio.charset.Charset;
-import java.util.UUID;
-
-public record AskForSeedsPayload(UUID uuid, ResourceLocation levelResource) implements CustomPacketPayload {
+public record AskForSeedsPayload() implements CustomPacketPayload {
     public static final CustomPacketPayload.Type<AskForSeedsPayload> TYPE = new CustomPacketPayload.Type<>(ResourceLocation.fromNamespaceAndPath(CozyCafes.MODID, "ask_for_seeds"));
 
     public static final StreamCodec<ByteBuf, AskForSeedsPayload> STREAM_CODEC = new StreamCodec<>() {
         @Override
         public AskForSeedsPayload decode(ByteBuf buffer) {
-            return new AskForSeedsPayload(FriendlyByteBuf.readUUID(buffer), ResourceLocation.parse(buffer.readCharSequence(buffer.readInt(), Charset.defaultCharset()).toString()));
+            return new AskForSeedsPayload();
         }
 
         @Override
         public void encode(ByteBuf buffer, AskForSeedsPayload value) {
-            FriendlyByteBuf.writeUUID(buffer, value.uuid);
-            String resourceString = value.levelResource.toString();
-            buffer.writeInt(resourceString.length());
-            buffer.writeCharSequence(resourceString, Charset.defaultCharset());
+            // do nothing
         }
     };
 
